56e571c3f6839ec9bcfb6eebcbff4a675d301e6a
tommy <tommysolano18@hotmail.com>
2021-11-15 15:43:12 -0500

    introduccion

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Switch, Route, Link, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" >Inicio</Link>
+          </li>
+          <li>
+            <Link to="/portafolio" >Portafolio</Link>
+          </li>
+        </ul>
+      </nav>
+      <section>
+        <Switch>
+          <Route exact path="/">
+            <h1>Inicio</h1>
+          </Route>
+          <Route exact path="/portafolio">
+            <h1>Portafolio</h1>
+          </Route>
+        </Switch>
+      </section>
+    </div>
+  );
+}
+
+export default App;
