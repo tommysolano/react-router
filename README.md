aec1ba3ff80cbc0234e7086e0dca105a581f922a
tommy <tommysolano18@hotmail.com>
2021-11-15 16:01:24 -0500

    documentacion react-router-6

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 practica react router
+
+doc react-router-dom v6
+https://dev.to/amarjits/basic-guide-to-use-react-router-v6-35do
--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
-import { Switch, Route, Link, Routes } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
+
+const Portafolio = () => {
+    return (
+        <div>
+            <h1>Portafolio</h1>
+            <ul>
+                <li>
+                    <Link to={"/portafolio/proyecto-1"}>Proyecto 1</Link>
+                </li>
+            </ul>
+            <div>
+                <Switch>
+                    <Route path={"/portafolio/proyecto-1"}>
+                        <h2>Proyecto 1</h2>
+                    </Route>
+                </Switch>
+            </div>
+        </div>
+    )
+}
 
 function App() {
   return (
@@ -19,7 +39,7 @@ function App() {
             <h1>Inicio</h1>
           </Route>
           <Route exact path="/portafolio">
-            <h1>Portafolio</h1>
+            <Portafolio/>
           </Route>
         </Switch>
       </section>
